c1fab9e7e66edf9cfd7f911d6f752ca9db5de25c
Izundu Kingsley <izundukingsleyemeka@gmail.com>
2026-01-17 21:19:37 +0100

    update deploy settings

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: parseInt(process.env.PORT) || 5173
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173
+    port: parseInt(process.env.PORT) || 4173
   },
   build: {
     outDir: 'dist'
