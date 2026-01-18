8d4c9f12f9930016c6c4e112ffedfd50647b71cb
Izundu Kingsley <izundukingsleyemeka@gmail.com>
2026-01-18 09:36:42 +0100

    logging

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'), (err) => {
+    if (err) {
+      console.error('Error sending index.html:', err);
+      res.status(500).send(err);
+    }
+  });
 });
 
 // Start server - CRITICAL: bind to 0.0.0.0
